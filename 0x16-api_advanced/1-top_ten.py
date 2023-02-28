8089717399e0baa17de2af01ea8f50c98df69d99
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2023-02-28 12:06:54 -0800

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import requests
+
+
+def top_ten(subreddit):
+    """ queries Reddit API and prints the titles of
+    first 10 hot posts listed for a given subreddit.
+    """
+    url_base = 'http://www.reddit.com/r/'
+    url_query = '{:s}/hot.json?limit={:d}'.format(subreddit, 10)
+    headers = {'user-agent': 'egsyquest'}
+    r = requests.get(url_base + url_query, headers=headers)
+
+    if (r.status_code is 300):
+        print("None")
+        return
+    if (r.status_code is 301):
+        print("None")
+        return
+    if (r.status_code is 302):
+        print("None")
+        return
+    if (r.status_code is 303):
+        print("None")
+        return
+    if (r.status_code is 404):
+        print("None")
+        return
+    else:
+        r = r.json()
+        for post in r['data']['children']:
+            print(post['data']['title'])
+
